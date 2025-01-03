faab8d4060de09c58bca209f29c95a3c9e920632
Александр <alexco@list.ru>
2025-01-03 18:56:40 +0300

    Update user.py

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
+# APIRouter с префиксом '/user' и тегом 'user'
 router = APIRouter(prefix='/user', tags=['user'])
 
+# 5 маршрутов c функциями all_users, user_by_id, create_user, update_user, delete_user
 @router.get('/')
 async def all_users():
     pass
