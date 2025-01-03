e82b82a2a83451dae9fc1ee61de4e123dc926954
Александр <alexco@list.ru>
2025-01-03 18:54:41 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 from routers import task, user
 
+# Приложение(объект) FastAPI
 app = FastAPI()
 
+# маршрут для app - '/', по которому функция возвращает словарь
 @app.get("/")
 async def welcome():
     return {"message": "Welcome to Taskmanager"}
 
+# Импорт объектов APIRouter и подключение к ранее созданному
+# приложению FastAPI, объединив все маршруты в одно приложение.
 app.include_router(task.router)
 app.include_router(user.router)
